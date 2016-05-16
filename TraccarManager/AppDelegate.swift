abc971eba027ef78ce09eb674973d884f0416146
Will Pearse <w.pearse@gmail.com>
2016-05-16 15:16:36 +1200

    Only reconnect web socket when auth'd

--- TraccarManager/AppDelegate.swift
+++ TraccarManager/AppDelegate.swift
@@ -16,7 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        WebService.sharedInstance.reconnectWebSocket()
+        if User.sharedInstance.isAuthenticated {
+            WebService.sharedInstance.reconnectWebSocket()
+        }
         
         return true
     }
