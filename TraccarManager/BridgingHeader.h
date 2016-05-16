8818759f7d0d0abbae165eeb30c6175e9b562fd3
Will Pearse <w.pearse@gmail.com>
2016-05-16 15:16:23 +1200

    Show spinner on login

---
+++ TraccarManager/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  TraccarManager
+//
+//  Created by William Pearse on 13/05/16.
+//  Copyright © 2016 Anton Tananaev. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "MBProgressHud.h"
+
+#endif /* BridgingHeader_h */
