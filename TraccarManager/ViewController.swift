a76b959f26cc4417eae8e63b4570a11104a9c1ea
Anton Tananaev <anton.tananaev@gmail.com>
2016-11-25 23:35:15 +1300

    Add a simple web view

--- TraccarManager/ViewController.swift
+++ TraccarManager/ViewController.swift
@@ -1,25 +1,43 @@
 //
-//  ViewController.swift
-//  TraccarManager
+// Copyright 2016 Anton Tananaev (anton.tananaev@gmail.com)
 //
-//  Created by Anton Tananaev on 2/03/16.
-//  Copyright © 2016 Anton Tananaev. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let statusFrame = UIApplication.shared.statusBarFrame
+        var viewFrame = view.frame
+        viewFrame.origin.y = statusFrame.size.height
+        viewFrame.size.height -= statusFrame.size.height
+        
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: viewFrame, configuration: webConfiguration)
+        webView.uiDelegate = self
+        
+        view.addSubview(webView)
+        
+        if let url = URL(string: "http://demo.traccar.org") {
+            self.webView.load(URLRequest(url: url))
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
